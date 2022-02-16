a7a42f49759d828fc42ad7823f631a46b7268e06
Tarequl Islam <tareque.dev@gmail.com>
2022-02-17 02:46:26 +0600

    Js calculation done

--- balance.js
+++ balance.js
@@ -1,22 +1,46 @@
      function getInputNumber(id) {
-          const income = document.getElementById(id).value;
-          const incomeValue = parseFloat(income);
+          const input = document.getElementById(id).value;
+          const inputValue = parseFloat(input);
+          if(inputValue != 'number' && inputValue < 0 && inputValue == '') {
+               alert('You should enter a number')
+          }
           document.getElementById(id).value = '';
-          return incomeValue;
+          return inputValue;
      }
-     // calculate income and saving
+     // calculate income and expense
      document.getElementById('calculate-btn').addEventListener('click', function() {
          const incomeValue = getInputNumber('income-value');
          const foodValue = getInputNumber('food-value');
          const rentValue = getInputNumber('rent-value');
          const clothValue = getInputNumber('cloth-value');
 
+         if(incomeValue < foodValue){
+          alert('Food expense never more than income value');
+         }else if(incomeValue < rentValue){
+          alert('Rent expense never more than income value')
+         }else if(incomeValue < clothValue){
+          alert('Cloths expense never more than income value')
+         }
         const totalExpenses =  foodValue + rentValue + clothValue;
         const balance = incomeValue - totalExpenses;
 
          document.getElementById('balance').innerText = balance;
          document.getElementById('total-expense').innerText = totalExpenses ;
       })   
+
+
+      // saving and remaining balance
+      document.getElementById('save-btn').addEventListener('click', function() {
+         const saveValue = getInputNumber('save-value');
+         const saving = balance.innerText;
+         const savingNumber = parseFloat(saving);
+         const savingRate =  savingNumber / 100;
+         const savingAmount = savingRate * saveValue;
+         const remainingBalance = savingNumber - savingAmount;
+       
+         document.getElementById('saving-amount').innerText = Math.round(savingAmount) ;
+         document.getElementById('remaining-balance').innerText = Math.round(remainingBalance) ;
+      })
          
 
 
